9e777d252675f9be21cdd5f89c928af2dc66cb4d
Lucas Torri <lucastorri@gmail.com>
2012-01-06 16:32:55 -0200

    incrementing dist script

--- dist.sh
+++ dist.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
+
+rm -rf dist
 play dist
+cd `dirname "$0"`/dist
+ZIP=`ls -1 | grep .zip`
+UNZIP_DIR=`echo $ZIP | sed 's/\(.*\)\.zip*/\1/'`
+DIST_ZIP=`echo $ZIP | sed 's/\(.*\)-.*/\1.zip/'`
+
+unzip $ZIP
+rm $ZIP
+mv $UNZIP_DIR/* .
+rmdir $UNZIP_DIR
+mv lib lib-web
+rm start
+cp -r ../bin .
+
+zip -r $DIST_ZIP *
+
+cd -
