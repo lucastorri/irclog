b9ac9208f630024bd135f40505e1681ebb3062b9
Lucas Torri <lucastorri@gmail.com>
2012-02-10 16:38:56 -0200

    trying to migrate to akka 2.0

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,47 +1,41 @@
 package controllers
 
 import models._
-import play.api.libs.iteratee.Input.EOF
-import play.api.mvc.Action
-import play.api.mvc.Controller
-import play.api.mvc.WebSocket
-import play.api.mvc.AsyncResult
+import play.api.mvc._
 
-import akka.actor.Actor.remote
-import akka.actor.Actor.toAnyOptionAsTypedOption
-import akka.actor.actorRef2Scala
-import akka.actor.ActorRef
-import play.api.libs.akka._
 import play.api.libs.concurrent._
-import co.torri.reindxr.index.SearchIndex
-import co.torri.reindxr.index.HighlightResult
-import co.torri.reindxr.index.SearchIndexResult
+import play.api.libs.concurrent.Akka._
+import play.api.Play.current
+import play.api.libs.iteratee._
+import co.torri.reindxr.index._
 import co.torri.jsonr._
 
 
 object Application extends Controller {
-	
-	private val indexedData = remote.actorFor("search-service", "localhost", 8123)
-  
-	def index = Action {
-    	Ok(views.html.index())
-  	}
-	
-  	def search = WebSocket[String] { request => (in, out) =>
-	  	out <<: in.map {
-		  	case EOF => EOF
-	  	  	case query => query.map { q => 
-				SearchResponse((indexedData ? SearchIndex(q)).as[SearchIndexResult])
-		  	}
-	  	}
-  	}
-	
-	def log(file: String, query: String) = Action {
-		AsyncResult {
-			(indexedData ? HighlightResult(query, file)).mapTo[String].asPromise.map { result =>
-				Ok(views.html.log(result))
-			}
-		}
-	}
-	
+
+  private val indexedData = Akka.system.actorFor("akka://search-service@localhost:8123")
+
+  def index = Action {
+    Ok(views.html.index())
+  }
+
+  //def search = WebSocket[String] { request => (in, out) =>
+    //out <<: in.map {
+      //case EOF => EOF
+      //case query => query.map { q => 
+        //SearchResponse((indexedData ? SearchIndex(q)).as[SearchIndexResult])
+      //}
+    //}
+  //}
+
+  def search = WebSocket.using[String] { request => (null, null) }
+
+  def log(file: String, query: String) = Action {
+    AsyncResult {
+      (indexedData ? HighlightResult(query, file)).mapTo[String].asPromise.map { result =>
+        Ok(views.html.log(result))
+      }
+    }
+  }
+
 }
--- project/Build.scala
+++ project/Build.scala
@@ -8,27 +8,12 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0"
 
     val appDependencies = Seq(
-        /*"org.apache.lucene" % "lucene-core" % "3.3.0",
-        "org.apache.lucene" % "lucene-highlighter" % "3.3.0",
-        "se.scalablesolutions.akka" % "akka-actor" % "1.1.3",*/
-        "se.scalablesolutions.akka" % "akka-remote" % "1.3-RC2"/*,
-        "org.slf4j" % "slf4j-api" % "1.6.1",
-        "ch.qos.logback" % "logback-classic" % "0.9.29",
-        "org.clapper" %% "grizzled-slf4j" % "0.6.6",
-        "org.streum" % "configrity_2.9.0" % "0.7.0",
-        "org.apache.yoko" % "yoko-spec-corba" % "1.3"*/
+        //"com.typesafe.akka" % "akka-actor" % "2.0-M4",
+        "com.typesafe.akka" % "akka-remote" % "2.0-M4"
     )
-    
-    resolvers += "Sonatype OSS Snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/"
-
-    resolvers += "Akka Maven2 Repository" at "http://akka.io/repository/"
-
-	resolvers += "Scala Tools Snapshots" at "http://scala-tools.org/repo-snapshots/"
-
-	resolvers += "GuiceyFruit Release Repository" at "http://guiceyfruit.googlecode.com/svn/repo/releases/"
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      // Add your own project settings here      
+      // Add your own project settings here
     )
 
 }
